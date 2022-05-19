45e432d752340526d8980cc489e11e2e82ec4f64
Antranik BABOYAN <antranik.baboyan@soprasteria.com>
2022-05-19 16:43:03 +0200

    init projet

---
+++ src/main/java/shakhmat_group/shakhmat/ShakhmatApplication.java
@@ -0,0 +1,13 @@
+package shakhmat_group.shakhmat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShakhmatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShakhmatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shakhmat_group/shakhmat/ShakhmatApplicationTests.java
@@ -0,0 +1,13 @@
+package shakhmat_group.shakhmat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShakhmatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
